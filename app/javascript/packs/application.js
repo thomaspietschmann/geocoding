86d086ea816cb78788a8e050e601f442711b5bb5
Thomas Pietschmann <thomas@pietschi.com>
2021-02-21 20:27:31 +0100

    css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,7 +28,7 @@ import { initAutocomplete } from "../plugins/init_autocomplete";
 import { initFetchFlats } from "../plugins/init_fetch_flats";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import "mapbox-gl/dist/mapbox-gl.css";
+
 
 flatpickr(".datepicker", {
   altInput: true,
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,7 @@
 // import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const initMapbox = () => {
   const mapElement = document.getElementById("map");
